0389059d80d6c52ce9c983f6c29342da1fa9c62d
tahmi matti <tahmi.matti01@gmail.com>
2022-06-17 14:12:00 +0200

    Affichage de l'API dans la pages d'accueil

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,17 +1,29 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component, useEffect, useState } from 'react';
+import axios from "axios";
 
-export class Countries extends Component {
-  render() {
+
+const Countries = () => {
+
+    const [data, setData] = useState([]);
+    // declaration d'une variable 
+    const [hello, setHello] = useState(['Hello My friends']);
     // Le useEffect se joue lorsque le composant est mis en place 
-    useEffect(()=>{
-
-    }, [])
+    useEffect(() => {
+        axios
+            .get("https://restcountries.com/v3.1/all")
+            .then((res) => setData(res.data));
+    }, []);
     return (
-      <div className='countries'>
-        <h1>Countries</h1>
+        <div className='countries'>
+            <h1>Countries</h1>
+            <ul>
+                {
+                    data.map((country, index) => (<li key={index}>{country.translations.fra.common}</li>))
+                }
+            </ul>
         </div>
     )
-  }
 }
+
 
 export default Countries
